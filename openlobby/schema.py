d769cdc49ebf125cbf3704f60ee24837b5d538ab
Jan Bednařík <jan.bednarik@gmail.com>
2017-11-22 11:03:42 +0100

    Extract types from schema.py into types.py

--- openlobby/schema.py
+++ openlobby/schema.py
@@ -1,59 +1,13 @@
 import re
-from elasticsearch import NotFoundError
 import graphene
 from graphene import relay
-from graphene.types.json import JSONString
 
-from .documents import UserDoc, ReportDoc, SessionDoc
+from .types import Report, User
+from .documents import UserDoc
 from .paginator import Paginator
 from .mutations import Mutation
+from .utils import get_viewer
 from . import search
-
-
-class Report(graphene.ObjectType):
-    author = graphene.Field(lambda: User)
-    date = graphene.String()
-    published = graphene.String()
-    title = graphene.String()
-    body = graphene.String()
-    received_benefit = graphene.String()
-    provided_benefit = graphene.String()
-    extra = JSONString()
-
-    class Meta:
-        interfaces = (relay.Node, )
-
-    @classmethod
-    def from_es(cls, report, author=None):
-        return cls(
-            id=report.meta.id,
-            author=author,
-            date=report.date,
-            published=report.published,
-            title=report.title,
-            body=report.body,
-            received_benefit=report.received_benefit,
-            provided_benefit=report.provided_benefit,
-            extra=report.extra._d_,
-        )
-
-    @classmethod
-    def get_node(cls, info, id):
-        try:
-            report = ReportDoc.get(id, using=info.context['es'])
-        except NotFoundError:
-            return None
-
-        author_type = cls._meta.fields['author'].type
-        author = author_type.get_node(info, report.author_id)
-        return cls.from_es(report, author)
-
-
-class ReportConnection(relay.Connection):
-    total_count = graphene.Int()
-
-    class Meta:
-        node = Report
 
 
 class SearchReportsConnection(relay.Connection):
@@ -61,49 +15,6 @@ class SearchReportsConnection(relay.Connection):
 
     class Meta:
         node = Report
-
-
-class User(graphene.ObjectType):
-    name = graphene.String()
-    openid_uid = graphene.String()
-    email = graphene.String()
-    extra = JSONString()
-    reports = relay.ConnectionField(ReportConnection)
-
-    class Meta:
-        interfaces = (relay.Node, )
-
-    @classmethod
-    def from_es(cls, user):
-        return cls(
-            id=user.meta.id,
-            name=user.name,
-            openid_uid=user.openid_uid,
-            email=user.email,
-            extra=user.extra._d_,
-        )
-
-    @classmethod
-    def get_node(cls, info, id):
-        try:
-            user = UserDoc.get(id, using=info.context['es'])
-        except NotFoundError:
-            return None
-        return cls.from_es(user)
-
-    def resolve_reports(self, info, **kwargs):
-        paginator = Paginator(**kwargs)
-        response = search.reports_by_author(info.context['es'], self.id, paginator)
-        total = response.hits.total
-        page_info = paginator.get_page_info(total)
-
-        edges = []
-        for i, report in enumerate(response):
-            cursor = paginator.get_edge_cursor(i + 1)
-            node = Report.from_es(report, author=self)
-            edges.append(ReportConnection.Edge(node=node, cursor=cursor))
-
-        return ReportConnection(page_info=page_info, edges=edges, total_count=total)
 
 
 def get_authors(es, ids):
@@ -135,13 +46,7 @@ class Query(graphene.ObjectType):
         return SearchReportsConnection(page_info=page_info, edges=edges, total_count=total)
 
     def resolve_viewer(self, info, **kwargs):
-        session_id = info.context.get('session_id', None)
-        if session_id is None:
-            return None
-        session = SessionDoc.get_active(info.context['es'], session_id)
-        if session is None:
-            return None
-        return User.get_node(info, session.user_id)
+        return get_viewer(info)
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation, types=[User, Report])
---
+++ openlobby/utils.py
@@ -0,0 +1,19 @@
+from flask import g
+
+from .documents import SessionDoc
+from .types import User
+
+
+def get_viewer(info):
+    """Resolves actual viewer and caches it into 'g'."""
+    if not hasattr(g, 'viewer'):
+        session_id = g.get('session_id', None)
+        if session_id is None:
+            g.viewer = None
+        else:
+            session = SessionDoc.get_active(info.context['es'], session_id)
+            if session is None:
+                g.viewer = None
+            else:
+                g.viewer = User.get_node(info, session.user_id)
+    return g.viewer
