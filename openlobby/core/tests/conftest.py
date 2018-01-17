5efadb9214d4e7dc25adf86faba78b3950e60095
Jan Bednařík <jan.bednarik@gmail.com>
2018-01-17 11:44:18 +0100

    Fix tests.

--- openlobby/core/tests/conftest.py
+++ openlobby/core/tests/conftest.py
@@ -1,11 +1,6 @@
 from elasticsearch import Elasticsearch
 import os
 import pytest
-import random
-import string
-
-
-from ..management import init_alias
 
 
 @pytest.fixture(scope='session')
@@ -15,18 +10,3 @@ def es():
     es_client = Elasticsearch(es_dsn)
     yield es_client
     es_client.indices.delete('test_*')
-
-
-@pytest.fixture
-def index_name():
-    """Random test index name."""
-    length = 10
-    word = ''.join(random.choice(string.ascii_lowercase) for i in range(length))
-    return 'test_{}'.format(word)
-
-
-@pytest.fixture
-def index(es, index_name):
-    """Initialized index."""
-    init_alias(es, index_name)
-    return index_name
