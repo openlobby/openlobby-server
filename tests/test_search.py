2e7baa5b77647478794d36f125d07071d627ad9a
Jan Bednařík <jan.bednarik@gmail.com>
2018-02-01 00:57:38 +0100

    Test search reports_by_author.

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,7 +1,7 @@
 import pytest
 
 from openlobby.core.api.paginator import Paginator, encode_cursor
-from openlobby.core.search import query_reports
+from openlobby.core.search import query_reports, reports_by_author
 
 from .dummy import prepare_reports
 
@@ -41,4 +41,24 @@ def test_query_reports__pagination(first, after, expected_ids):
     query = ''
     paginator = Paginator(first=first, after=after)
     response = query_reports(query, paginator)
+    assert expected_ids == [int(r.meta.id) for r in response]
+
+
+def test_reports_by_author():
+    prepare_reports()
+    author_id = 1
+    paginator = Paginator()
+    response = reports_by_author(author_id, paginator)
+    assert [3, 1] == [int(r.meta.id) for r in response]
+
+
+@pytest.mark.parametrize('first, after, expected_ids', [
+    (1, None, [3]),
+    (1, encode_cursor(1), [1]),
+])
+def test_reports_by_author__pagination(first, after, expected_ids):
+    prepare_reports()
+    author_id = 1
+    paginator = Paginator(first=first, after=after)
+    response = reports_by_author(author_id, paginator)
     assert expected_ids == [int(r.meta.id) for r in response]
