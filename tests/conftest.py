cc2d2278b4b395468bf0871fd0311868e79058e4
Jan Bednařík <jan.bednarik@gmail.com>
2018-02-08 01:10:56 +0100

    Keycloak test fixture.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import dsnparse
 from django_elasticsearch_dsl.test import ESTestCase
 
 
@@ -16,8 +17,33 @@ class TestCase(ESTestCase, DummyTestCase):
 
 @pytest.fixture
 def django_es():
-    """Setup and teardown of test indices."""
+    """Setup and teardown of Elasticsearch test indices."""
     testCase = TestCase()
     testCase.setUp()
     yield
     testCase.tearDown()
+
+
+def pytest_addoption(parser):
+    parser.addoption('--keycloak', action='store',
+        help=('Keycloak server DSN with OpenID client credentials for auth tests.'
+              ' Format: http://client_id:client_secret@host:port/realm'))
+
+
+@pytest.fixture(scope='session')
+def keycloak(request):
+    """Keycloak server and OpenID client info."""
+    dsn = request.config.getoption('--keycloak')
+    if dsn is None:
+        pytest.skip('No Keycloak DSN provided.')
+
+    config = dsnparse.parse(dsn)
+    realm_url = '{}://{}:{}/auth/realms/{}'.format(config.scheme, config.host, config.port, config.paths[0])
+    return {
+        'client_id': config.username,
+        'client_secret': config.password,
+        'issuer': realm_url,
+        'authorization_endpoint': '{}/protocol/openid-connect/auth'.format(realm_url),
+        'token_endpoint': '{}/protocol/openid-connect/token'.format(realm_url),
+        'userinfo_endpoint': '{}/protocol/openid-connect/userinfo'.format(realm_url),
+    }
