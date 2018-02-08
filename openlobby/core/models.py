b93be53f5aa7aff614c76b719f85a3388d0097ad
Jan Bednařík <jan.bednarik@gmail.com>
2018-02-08 12:11:36 +0100

    LoginAttempt model - change redirect_uri to app_redirect_uri.

--- openlobby/core/models.py
+++ openlobby/core/models.py
@@ -33,7 +33,7 @@ class LoginAttempt(models.Model):
     """Temporary login attempt details which persists redirects."""
     openid_client = models.ForeignKey(OpenIdClient, on_delete=models.CASCADE)
     state = models.CharField(max_length=50, unique=True, db_index=True)
-    redirect_uri = models.CharField(max_length=255)
+    app_redirect_uri = models.CharField(max_length=255)
     expiration = models.IntegerField()
 
 
