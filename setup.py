877308de1b19516a7599cec6f6293def054e34f5
Jan Bednařík <jan.bednarik@gmail.com>
2018-03-08 22:56:18 +0100

    Setup.py update.

--- setup.py
+++ setup.py
@@ -13,57 +13,30 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='openlobby',
     version='0.1.0',
+    url='https://github.com/openlobby/openlobby-server',
+
+    author='Jan Bednarik',
+    author_email='jan.bednarik@gmail.com',
+
     description='Open Lobby Server',
     long_description=long_description,
-    url='https://github.com/openlobby/openlobby-server',
-    author='Jan Bednařík',
-    author_email='jan.bednarik@gmail.com',
-    license='MIT',
+
     packages=find_packages(exclude=['tests']),
-)
 
-# TODO
-"""
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    # TODO
+    # install_requires=[],
+    # extras_require={
+    #     'dev': [],
+    #     'test': [],
+    # },
+
+    license='MIT',
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        # Pick your license as you wish (should match "license" above)
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
-
-    # What does your project relate to?
-    keywords='sample setuptools development',
-
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['peppercorn'],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    extras_require={
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
-    },
 )
-"""
